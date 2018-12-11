9eb6c19f2c01d6d2326bcd987219bf2bdbe90a7b
indichi1396 <indichi1396@gmail.com>
2018-12-11 19:03:37 +0900

    LOLAPI 메인 화면 버튼구현 및 디자인

--- LOLAPI/Form1.Designer.cs
+++ LOLAPI/Form1.Designer.cs
@@ -28,13 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(279, 330);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(559, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 58);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(153, 441);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SlateGray;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(153, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(647, 27);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel3.Location = new System.Drawing.Point(723, 1);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(77, 96);
+            this.panel3.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gray;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 110);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "챔피언\r\n(Champion)";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gray;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(0, 110);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 110);
+            this.button3.TabIndex = 1;
+            this.button3.Text = " 아이템\r\n ( item )";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gray;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(0, 220);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 110);
+            this.button4.TabIndex = 2;
+            this.button4.Text = " 스펠\r\n(spell)\r\n";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Gray;
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Location = new System.Drawing.Point(0, 330);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(153, 110);
+            this.button5.TabIndex = 3;
+            this.button5.Text = " 룬\r\n(rune)";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 441);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ㅗ";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- LOLAPI/Form1.cs
+++ LOLAPI/Form1.cs
@@ -16,5 +16,64 @@ namespace LOLAPI
         {
             InitializeComponent();
         }
+        FrmSpell fs;
+        FrmRune fr;
+        FrmItems fi;
+        FrmChampion fc;
+        //private void 스펠SpellToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    if (fs == null)
+        //    {
+        //        fs = new FrmSpell();
+        //        fs.MdiParent = this;
+        //        fs.Show();
+        //    }
+        //    else
+        //    {
+        //        fs.Focus();
+        //    }
+        //}
+
+        //private void 챔피언ChampionToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    if (fc == null)
+        //    {
+        //        fc = new FrmChampion();
+        //        fc.MdiParent = this;
+        //        fc.Show();
+        //    }
+        //    else
+        //    {
+        //        fc.Focus();
+        //    }
+        //}
+
+        //private void 룬RuneToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    if (fr == null)
+        //    {
+        //        fr = new FrmRune();
+        //        fr.MdiParent = this;
+        //        fr.Show();
+        //    }
+        //    else
+        //    {
+        //        fr.Focus();
+        //    }
+        //}
+
+        //private void 아이템ItemToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    if (fi == null)
+        //    {
+        //        fi = new FrmItems();
+        //        fi.MdiParent = this;
+        //        fi.Show();
+        //    }
+        //    else
+        //    {
+        //        fi.Focus();
+        //    }
+        //}
     }
 }
